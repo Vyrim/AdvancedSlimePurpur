1b3f5fd9be3b1a771115518b51ecd3707cd05f75
Sven Derichs <kleinesNugget@gmail.com>
2023-05-20 21:56:56 +0200

    Switched to Purpur (68117a4e35802c17c89eb0b8ff993d08a8e2bd66) -> gradle.properties

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -11,84 +11,15 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
 }
 
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/nms/")
+    maven("https://repo.rapture.pw/repository/maven-releases/")
+    maven("https://repo.glaremasters.me/repository/concuncan/")
+    maven("https://jitpack.io/")
+}
+
 java {
     withSourcesJar()
     withJavadocJar()
-}
-
-profiles {
-    profile("publish") {
-        activation {
-            property {
-                setKey("publish")
-                setValue("true")
-            }
-        }
-        action {
-            publishing {
-                publications {
-                    create<MavenPublication>("maven") {
-                        groupId = "${project.group}"
-                        artifactId = project.name
-                        version = "${project.version}"
-
-                        from(components["java"])
-
-                        pom {
-                            name.set("Advanced Slime World Manager API")
-                            description.set("API for ASP")
-                            url.set("https://github.com/InfernalSuite/AdvancedSlimePaper")
-                            licenses {
-                                license {
-                                    name.set("GNU General Public License, Version 3.0")
-                                    url.set("https://www.gnu.org/licenses/gpl-3.0.txt")
-                                }
-                            }
-                            developers {
-                                developer {
-                                    id.set("InfernalSuite")
-                                    name.set("The InfernalSuite Team")
-                                    url.set("https://github.com/InfernalSuite")
-                                    email.set("infernalsuite@gmail.com")
-                                }
-                            }
-                            scm {
-                                connection.set("scm:git:https://github.com:InfernalSuite/AdvancedSlimePaper.git")
-                                developerConnection.set("scm:git:ssh://github.com:InfernalSuite/AdvancedSlimePaper.git")
-                                url.set("https://github.com/Paul19988/Advanced-Slime-World-Manager/")
-                            }
-                            issueManagement {
-                                system.set("Github")
-                                url.set("https://github.com/InfernalSuite/AdvancedSlimePaper/issues")
-                            }
-                        }
-
-                        versionMapping {
-                            usage("java-api") {
-                                fromResolutionOf("runtimeClasspath")
-                            }
-                            usage("java-runtime") {
-                                fromResolutionResult()
-                            }
-                        }
-                    }
-                }
-                repositories {
-                    maven {
-                        name = "infernalsuite"
-                        url = uri("https://repo.infernalsuite.com/repository/maven-snapshots/")
-                        credentials {
-                            username = project.property("ISUsername") as String?
-                            password = project.property("ISPassword") as String?
-                        }
-                    }
-                }
-            }
-
-            signing {
-                useGpgCmd()
-                sign(publishing.publications["maven"])
-            }
-        }
-    }
 }
